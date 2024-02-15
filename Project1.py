789fca2c7b38b18cf4b11fc2b58c41c22c9c578e
Sundeepa Singh <56774147+ASI11006@users.noreply.github.com>
2024-02-16 06:35:02 +1100

    Add files via upload

---
+++ Project1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+import pandas as pd
+
+
+# In[4]:
+
+
+import matplotlib.pyplot as plt
+
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[12]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
